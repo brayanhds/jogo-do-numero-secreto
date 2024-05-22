5f77e20dfc5115ad431eecc12b751605d9c438a1
Brayan Santos <brayyansantoss@gmail.com>
2024-05-21 21:20:37 -0300

    projeto inicial

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaNumerosSorteados = [];
 let numeroLimite = 10;
-let numeroSecreto = 10;
+let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirNaTela (tag, texto) {
